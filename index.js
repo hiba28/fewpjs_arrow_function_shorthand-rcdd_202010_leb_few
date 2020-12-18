3eb1bd4cbc055d2e65cc6bce177f601379b22f25
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-18 05:16:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let divide = function(a,b){
   return b != 0 ? a/b : "b is invalid"
 }
-let arr = [1,2,3]
-let newArr = arr.map(elem => elem*2)
-
-let arrow_function = (a,b) => a+b
+let square = elem => elem*elem
+let add = (a,b) => a+b
